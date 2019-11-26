7df3c16de9757e7ff5c1cc6b6c51012dedc90763
Stefan Kohlmeier <StefanKohlmeier123@t-online.de>
2019-11-26 11:10:59 +0100

    Server initial commit

--- server/Server.java
+++ server/Server.java
@@ -1,3 +1,26 @@
 package server;
 
-public class Server {}
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Hashtable;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+    private HashSet<Socket> clientSockets;
+
+    public Server(int port)
+    {
+        try
+        {
+            serverSocket = new ServerSocket(port);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
