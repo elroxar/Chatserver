0b0dde742bd6e6a3819232f6405b243901829ead
Theo Thiedig <theo@thiedig.net>
2019-11-27 08:40:34 +0100

    Database Connector and Config Files

--- server/DBConnector.java
+++ server/DBConnector.java
@@ -1,3 +1,30 @@
 package server;
 
-public class DBConnector {}
+import org.mariadb.jdbc.MariaDbDataSource;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBConnector {
+	
+	private final static String database = "localhost";
+	private final static int port = 3306;
+	private MariaDbDataSource conn;
+	private Logger logger;
+	
+	public DBConnector(String pUser, String pPassword){
+		conn = new MariaDbDataSource();
+		logger = Logger.getLogger("databse");
+		try{
+			conn.setServerName(database);
+			conn.setUser(pUser);
+			conn.setPort(port);
+			conn.setPassword(pPassword);
+			conn.getConnection();
+			logger.log(Level.INFO, String.format("Connecting to resource jdbc://%s:%s@%s:%s", database, port, pUser, pPassword));
+		}
+		catch(SQLException se){se.printStackTrace();}
+		finally{logger.log(Level.INFO, "Connected to resource.");}
+	}
+}
---
+++ server/Host.java
@@ -0,0 +1,9 @@
+package server;
+
+public class Host {
+	
+	public static void main(String[] args){
+		new DBConnector("theo", "!2E45sex");
+		
+	}
+}
